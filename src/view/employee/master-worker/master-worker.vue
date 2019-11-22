0eff1e2d48f511d00df688007f32e88ad2aa008f
zxj <zxj@qq.com>
2019-11-22 21:00:26 +0800

    -

---
+++ src/view/employee/master-worker/master-worker.vue
@@ -0,0 +1,69 @@
+<template>
+  <Table :columns="columns1" :data="data1"></Table>
+</template>
+
+<script>
+  export default {
+    name: "master-worker",
+    data() {
+      return {
+        columns1: [
+          {
+            title: '姓名',
+            key: 'name'
+          },
+          {
+            title: '年龄',
+            key: 'age'
+          },
+          {
+            title: '电话',
+            key: 'age'
+          },
+          {
+            title: '地址',
+            key: 'address'
+          },
+          {
+            title: '票数',
+            key: 'age'
+          },
+          {
+            title: '是否在职',
+            key: 'age'
+          },
+        ],
+        data1: [
+          {
+            name: 'John Brown',
+            age: 18,
+            address: 'New York No. 1 Lake Park',
+            date: '2016-10-03'
+          },
+          {
+            name: 'Jim Green',
+            age: 24,
+            address: 'London No. 1 Lake Park',
+            date: '2016-10-01'
+          },
+          {
+            name: 'Joe Black',
+            age: 30,
+            address: 'Sydney No. 1 Lake Park',
+            date: '2016-10-02'
+          },
+          {
+            name: 'Jon Snow',
+            age: 26,
+            address: 'Ottawa No. 2 Lake Park',
+            date: '2016-10-04'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
