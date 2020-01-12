f2c5d3cce5e16c9f173b74f5ee8f6b580c78d1f4
zxj <zxj@qq.com>
2020-01-12 21:34:04 +0800

    '-'

--- src/components/main/components/hint-audio/hint-audio.vue
+++ src/components/main/components/hint-audio/hint-audio.vue
@@ -1,13 +1,33 @@
 <template>
-  <audio src="./ding_dong.mp3" autoplay="autoplay"></audio>
+  <audio ref="audio" class="hint-audio" loop controls>
+    <source :src="dingDong" type="audio/mpeg">
+  </audio>
 </template>
 
 <script>
-    export default {
-        name: "HintAudio"
+  import dingDong from './ding_dong.mp3'
+
+  export default {
+    name: "HintAudio",
+    data() {
+      return {
+        dingDong: dingDong
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        console.log(this.$refs.audio)
+        // this.$refs.audio.play();
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .hint-audio {
+    position: fixed;
+    visibility: hidden;
+    opacity: 0;
+    z-index: -1;
+  }
 </style>
