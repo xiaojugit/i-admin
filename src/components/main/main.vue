c98edcbfd1e0f5431706370d496c9e5a2ba49bf5
xiaoju <xiaoju.zh@qq.com>
2020-03-16 08:59:13 +0800

    -

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -50,8 +50,8 @@ import ErrorStore from './components/error-store'
 import { mapMutations, mapActions, mapGetters } from 'vuex'
 import { getNewTagList, routeEqual } from '@/libs/util'
 import routers from '@/router/routers'
-import minLogo from '@/assets/images/logo-min.jpg'
-import maxLogo from '@/assets/images/logo.jpg'
+import minLogo from '@/assets/images/basicprofile.jpg'
+import maxLogo from '@/assets/images/basicprofile.jpg'
 import './main.less'
 export default {
   name: 'Main',
